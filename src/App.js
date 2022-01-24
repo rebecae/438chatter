cc96568aac2abaa89f159cb9161206ef33ed424c
Rebeca <rebeca@Rebecas-MacBook-Pro.local>
2022-01-23 20:18:47 -0800

    complete

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'> 
+        <div className='logo'/>
+        <span className='title'>CHATTER!</span>
+      </header>
+      <footer className='footer'>
+        <input className='text-input' />
+        <button className='send'>↑</button>
+      </footer>
+     </div>
+  );
+}
+
+export default App;
