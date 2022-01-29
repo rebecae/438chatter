defe2b437073e99aeb3786f649bccaed9dd208e8
Rebeca <rebeca@Rebecas-MacBook-Pro.local>
2022-01-29 01:25:34 -0800

    E4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import TextInput from './TextInput';
+import { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+  
+  function sendMessage(msg) {
+    console.log(' MY MESSAGE', msg);
+    setMessage(msg);
+  }
+
   return (
     <div className="App">
       <header className='header'> 
         <div className='logo' />
         <span className='title'>CHATTER!</span>
       </header>
-      <TextInput />
+      <div className ='messages'> {message} </div>
+      <TextInput sendMessage={sendMessage} />
      </div>
   );
 }
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from 'react';
 import "./TextInput.css";
+import { useState } from "react";
 
-function TextInput() {
-    return (
-        <footer className='footer'>
-            <input className='text-input' />
-            <button className='send'>↑</button>
-        </footer>
-    );
+function TextInput(props) {
+  const [text, setText] = useState('');
+
+  function click() {
+    props.sendMessage(text);
+    setText("");
+  }
+
+	return (
+    <footer className='footer'>
+      <input 
+        className='text-input' 
+        value={text} 
+				onChange={(e) => setText(e.target.value)}  
+      />
+    	<button className='send' onClick={click}>
+      	↑
+    	</button>
+    </footer>
+	);
 }
 
 export default TextInput;
